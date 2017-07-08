adabaa4c4862e064f03b80cd06a5dd5bcaa778bb
Laurent Verdoïa <verdoialaurent@gmail.com>
2017-07-08 18:29:10 +0200

    Use C call convention for darwin_386

--- darwin_386/nasm.asm
+++ darwin_386/nasm.asm
@@ -4,44 +4,66 @@ global  start
 
 section .text
 
+; Program entry point
 start:
-    call    _print
+    push    dword msg.len ; Message length
+    push    dword msg ; Message pointer
+    push    dword 2 ; stdout
+    call    _write
+    add     esp, 12 ; Clean stack
+
+    push    dword eax
     call    _exit
+    ; There is no return from _exit
 
-; Print "Hello, world!\n" to STDOUT
-_print:
-    ; Prepare stack
-    push    dword msg.len
-    push    dword msg
-    push    dword 1
 
-    ; Set system call number
-    ; ssize_t write(int fd, const void *buf, size_t nbyte);
-    mov     eax, 4
+; Perform syscall number 4
+; ssize_t write(int fd, const void *buf, size_t nbyte)
+_write:
+    %stacksize flat
+    %arg fd:dword
+    %arg buf:dword
+    %arg nbyte:dword
+    enter   0, 0 ; Create a new stack frame
+
+    mov     eax, 4 ; Set syscall number
+
+    ; Prepare arguments stack for syscall
+    push    dword [nbyte]
+    push    dword [buf]
+    push    dword [fd]
 
     ; Perform system call
     call    _kernel
 
     ; Clean the stack
-    add     esp, 12
+    leave
 
     ret
 
-; Perform an _exit(12)
-_exit:
-    ; Prepare the stack
-    push    dword 12
 
-    ; Set system call number
-    ; void exit(int rval);
-    mov     eax, 1
+; Perform syscall number 1
+; void exit(int status)
+_exit:
+    %stacksize flat
+    %arg status:dword
+    enter   0,0 ; Create a new stack frame
+
+    mov     eax, 1 ; Set syscall number
+
+    ; Prepare arguments stack for syscall
+    push    dword [status] ;
 
     ; Perform system call
     call    _kernel
 
-    ret
+    ; There is not return from this syscall
+    ; No need to 'leave' or 'ret'
+
 
 ; Perform an system call with a prepared stack
+; Change of the prepared stack will break syscall ABI
+; => Do not create new stack frame
 _kernel:
     int     0x80
     ret
