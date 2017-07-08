bf5d059ad9541ccbb7de1cd81aeb1ebbc8263b9a
Laurent Verdoïa <verdoialaurent@gmail.com>
2017-07-08 23:08:17 +0200

    Use C call convention for darwin_amd64

--- darwin_amd64/nasm.asm
+++ darwin_amd64/nasm.asm
@@ -5,22 +5,45 @@ global start
 section .text
 
 start:
-    call    _print
+    mov     rdx, msg.len ; Message length
+    mov     rsi, msg ; Message pointer
+    mov     rdi, dword 1 ; stdout
+    call    _write
+
+    mov     rdi, rax
     call    _exit
 
-_print:
-    mov     rax, 0x2000004 ; write
-    mov     rdi, 1 ; stdout
-    mov     rsi, msg
-    mov     rdx, msg.len
+
+; Perform syscall number 4
+; ssize_t write(int fd, const void *buf, size_t nbyte)
+_write:
+    enter   0,0 ; Create a new stack frame
+
+    mov     rax, 0x2000004 ; Set syscal number
+
+    ; All parameters are setted with function call convention
+    ; Just perform system call
     syscall
+
+    ; Clean the stack
+    leave
+
     ret
 
+
+; Perform syscall number 1
+; void exit(int status)
 _exit:
-    mov     rax, 0x2000001 ; exit
-    mov     rdi, 12
+    enter   0,0 ; Create a new stack frame
+
+    mov     rax, 0x2000001 ; Set syscall number
+
+    ; All parameters are setted with function call convention
+    ; Just perform system call
     syscall
-    ret
+
+    ; There is not return from this syscall
+    ; No need to 'leave' or 'ret'
 
 section .data
 
