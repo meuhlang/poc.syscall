17d1ffb1cacf07926c9c74d68b9c25f117c42e62
Laurent Verdoïa <verdoialaurent@gmail.com>
2017-08-06 19:07:50 +0000

    Add sugar to install.ubuntu.sh

--- linux_amd64/install.ubuntu.sh
+++ linux_amd64/install.ubuntu.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# vim:set ts=4 sw=4 tw=100 et:
+
+set -o xtrace
+set -o errexit
+set -o pipefail
+set -o nounset
+
 packages=(
 	"vim"
 	"llvm-4.0"
@@ -10,3 +17,6 @@ packages=(
 sudo add-apt-repository universe
 sudo apt-get update
 sudo apt-get -y install "${packages[@]}"
+sudo update-alternatives --set editor /usr/bin/vim.basic
+
+git clone https://github.com/meuhlang/poc.syscall.git
